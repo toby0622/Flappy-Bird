7493b26d7e56a9b599858bab7629bfd1bd215540
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-07-23 16:13:28 +0800

    Game Over Setup

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Flappy Bird')
         self.clock = pygame.time.Clock()
+        self.active = True
         # Sprite Groups
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
@@ -28,17 +29,30 @@ class Game:
         # Text
         self.font = pygame.font.Font('graphics/font/BD_Cartoon_Shout.ttf', 30)
         self.score = 0
+        self.start_offset = 0
+        # Menu
+        self.menu_surf = pygame.image.load('graphics/ui/menu.png').convert_alpha()
+        self.menu_rect = self.menu_surf.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
     def collisions(self):
         if pygame.sprite.spritecollide(self.plane, self.collision_sprites, False, pygame.sprite.collide_mask) \
                 or self.plane.rect.top <= 0:
-            pygame.quit()
-            sys.exit()
+            for sprite in self.collision_sprites.sprites():
+                if sprite.sprite_type == 'obstacle':
+                    sprite.kill()
+
+            self.active = False
+            self.plane.kill()
 
     def display_score(self):
-        self.score = pygame.time.get_ticks() // 1000
+        if self.active:
+            self.score = (pygame.time.get_ticks() - self.start_offset) // 1000
+            y = WINDOW_HEIGHT / 10
+        else:
+            y = WINDOW_HEIGHT / 2 + (self.menu_rect.height / 1.5)
+
         score_surf = self.font.render(str(self.score), True, 'black')
-        score_rect = score_surf.get_rect(midtop=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 10))
+        score_rect = score_surf.get_rect(midtop=(WINDOW_WIDTH / 2, y))
         self.display_surface.blit(score_surf, score_rect)
 
     def run(self):
@@ -54,15 +68,26 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    self.plane.jump()
-                if event.type == self.obstacle_timer:
+                    if self.active:
+                        self.plane.jump()
+                    else:
+                        self.plane = Plane(self.all_sprites, self.scale_factor / 1.8)
+                        self.active = True
+                        self.start_offset = pygame.time.get_ticks()
+
+                if event.type == self.obstacle_timer and self.active:
                     Obstacle([self.all_sprites, self.collision_sprites], self.scale_factor * 1.1)
             # Game Logic
             self.display_surface.fill('black')
             self.all_sprites.update(dt)
-            self.collisions()
             self.all_sprites.draw(self.display_surface)
             self.display_score()
+
+            if self.active:
+                self.collisions()
+            else:
+                self.display_surface.blit(self.menu_surf, self.menu_rect)
+
             pygame.display.update()
             self.clock.tick(FRAMERATE)
 
--- sprites.py
+++ sprites.py
@@ -31,6 +31,7 @@ class Background(pygame.sprite.Sprite):
 class Ground(pygame.sprite.Sprite):
     def __init__(self, groups, scale_factor):
         super().__init__(groups)
+        self.sprite_type = 'ground'
         # Image
         ground_surface = pygame.image.load('graphics/environment/ground.png').convert_alpha()
         self.image = pygame.transform.scale(ground_surface,
@@ -105,6 +106,7 @@ class Plane(pygame.sprite.Sprite):
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self, groups, scale_factor):
         super().__init__(groups)
+        self.sprite_type = 'obstacle'
         orientation = choice(('up', 'down'))
         surface = pygame.image.load(f'graphics/obstacles/{choice((0, 1))}.png').convert_alpha()
         self.image = pygame.transform.scale(surface, pygame.math.Vector2(surface.get_size()) * scale_factor)
